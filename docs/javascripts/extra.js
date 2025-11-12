9600cd4ffbeaf5751f8b8f1b821aa07fcc341474
asantora05 <asantora05@gmail.com>
2025-11-11 21:52:47 -0500

    formatting

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,5 +1,5 @@
 // Custom JavaScript for grai.build documentation
 
-console.log('grai.build documentation loaded');
+console.log("grai.build documentation loaded");
 
 // Add any custom interactive features here
